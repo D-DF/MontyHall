1882c5f7f672ee18aaf7897751ea5d0114db565c
D-DF <daviddefreitas0192@gmail.com>
2021-04-08 21:24:40 +0200

    ok

--- montyHall.js
+++ montyHall.js
@@ -8,12 +8,9 @@ let voiture      = '🚘';
 let indexHasard  = randomInt(0, tableau.length); // index de la voiture 
 let chifChoice   = randomInt(0, tableau.length);
 let tabHasard    = tableau.splice(indexHasard, 1, voiture)
-let question     = 1
+let question     = ""
 let questionDeux = ""
-let count = 0
 
-
-do {
     question = readlineSync.question(`Quel porte? 
 Réponse : `)
 
@@ -22,33 +19,29 @@ Réponse : `)
 
     
     if(question !== indexHasard) {
-    count ++
     console.log(`Tu as retiré une : ... ${tableau[chifChoice]}`)
     tableau.splice(chifChoice, 1)
     } else if (question === indexHasard) {
-        count ++
     console.log(`Tu as retiré ${tableau[chifChoice]}`)
     tableau.splice(chifChoice, 1)
     console.log(test)
     }
 
-   console.log(tableau)
+    console.log(tableau)
     console.log(indexHasard)
 
     questionDeux = readlineSync.question(`Tu gardes ton choix ou change de portes? 
 Réponse : `)
 
     if(questionDeux == indexHasard) {
-        count ++
         console.log("Bravo")
         process.exit(1)
         } else {
-            count ++
         console.log(`Perdu, la bonne porte était la ${indexHasard}`)
         process.exit(1)
     }
 
-} while (count === 2)
+
 
       
         
